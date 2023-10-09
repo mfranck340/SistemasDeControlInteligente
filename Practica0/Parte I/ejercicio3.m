f67240cf485fe38e509305d33253da0be6278f08
mfranck340 <franck.yaguargos@edu.uah.es>
2023-10-09 14:11:49 +0200

    Pequeños arreglos ejercicio3

--- Practica0/Parte I/ejercicio3.m
+++ Practica0/Parte I/ejercicio3.m
@@ -9,29 +9,41 @@ esCuadrada = length(dimensiones) == 1;
 A = IntroducirMatriz(dimensiones);
 B = IntroducirMatriz(dimensiones);
 
-% 4. Mostrar por pantalla
-% Las matrices generadas
+% 4. Mostrar por pantalla las matrices generadas
+fprintf('\nMatrices generadas\n')
 disp('Matriz A:')
 disp(A)
 disp('Matriz B:')
 disp(B)
 
 % Traspuesta e inversa de cada matriz
+fprintf('\nTraspuesta e inversa de cada matriz\n')
 disp('Traspuesta de A:')
 disp(A')
 disp('Traspuesta de B:')
 disp(B')
 
+tolerancia = 1e-10;  % Define una tolerancia pequeña
+
 if esCuadrada
-  disp('Inversa de A:')
-  disp(inv(A))
-  disp('Inversa de B:')
-  disp(inv(B))
+  if abs(det(A)) < tolerancia
+    disp('No se puede calcular la inversa de una matriz singular')
+  else
+    disp('Inversa de A:')
+    disp(inv(A))
+  end
+  if abs(det(B)) < tolerancia
+    disp('No se puede calcular la inversa de una matriz singular')
+  else
+    disp('Inversa de B:')
+    disp(inv(B))
+  end
 else
   disp('No se puede calcular la inversa de una matriz no cuadrada')
 end
 
 % Determinante y rango de cada matriz
+fprintf('\nDeterminante y rango de cada matriz\n')
 if esCuadrada
   disp('Determinante de A:')
   disp(det(A))
@@ -47,6 +59,7 @@ disp('Rango de B:')
 disp(rank(B))
 
 % Producto matricial y elemento a elemento
+fprintf('\nProducto matricial y elemento a elemento\n')
 if esCuadrada
   disp('Producto matricial A*B:')
   disp(A*B)
@@ -58,9 +71,9 @@ disp('Producto elemento a elemento A.*B:')
 disp(A.*B)
 
 % Vector fila con la primera fila de ambas matrices
-disp('Vector fila:')
+fprintf('\nVector fila con la primera fila de ambas matrices\n')
 disp([A(1,:) B(1,:)])
 
 % Vector columna con la primera columna de ambas matrices
-disp('Vector columna:')
+fprintf('\nVector columna con la primera columna de ambas matrices\n')
 disp([A(:,1); B(:,1)])
