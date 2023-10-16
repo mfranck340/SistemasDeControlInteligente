e9c8eeccbc68af7c40b170fb7669ea50acebeffb
dani-mg-05 <danimg0502@gmail.com>
2023-10-16 22:03:59 +0200

    Ejercicio 3

--- Practica1/ejercicio3.m
+++ Practica1/ejercicio3.m
@@ -1,15 +1,19 @@
 % Carga de datos de ejemplo disponibles en la toolbox
-%[inputs,targets] = simplefit_dataset;
-[inputs,targets] = bodyfat_dataset;
+[inputs,targets] = simplefit_dataset;
+%[inputs,targets] = bodyfat_dataset;
 
 % Creación de la red
 hiddenLayerSize = 10;
 net = fitnet(hiddenLayerSize);
+%net = fitnet(hiddenLayerSize, 'trainrp');
+%net = fitnet(hiddenLayerSize, 'trainlm');
+%net = fitnet(hiddenLayerSize, 'traingd');
+%net = fitnet(hiddenLayerSize, 'trainbr');
 
 % División del conjunto de datos para entrenamiento, validación y test
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
+net.divideParam.trainRatio = 80/100;
+net.divideParam.valRatio = 10/100;
+net.divideParam.testRatio = 10/100;
 
 % Entrenamiento de la red
 [net,tr] = train(net,inputs,targets);
