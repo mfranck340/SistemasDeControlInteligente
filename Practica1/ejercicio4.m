9a9b82e707f468436912605ab16b927e59bdf5dc
mfranck340 <franck.yaguargos@edu.uah.es>
2023-10-16 20:08:03 +0200

    modificacion en el codigo

--- Practica1/ejercicio4.m
+++ Practica1/ejercicio4.m
@@ -1,15 +1,17 @@
+% Ejercicio 4: Clasificacion
+
 % Carga de datos de ejemplo disponibles en la toolbox
 %[inputs,targets] = simpleclass_dataset;
 [inputs,targets] = cancer_dataset;
 
 % Creción de una red neuronal para el reconocimiento de patrones
 hiddenLayerSize = 10;
-net = patternnet(hiddenLayerSize);
+net = patternnet(hiddenLayerSize, 'trainscg');
 
 % División del conjunto de datos para entrenamiento, validación y test
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
+net.divideParam.trainRatio = 80/100;
+net.divideParam.valRatio = 10/100;
+net.divideParam.testRatio = 10/100;
 
 % Entrenamiento de la red
 [net,tr] = train(net,inputs,targets);
