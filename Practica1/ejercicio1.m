70e9373a94a55925181794a4927618328b43280d
mfranck340 <franck.yaguargos@edu.uah.es>
2023-10-16 12:55:48 +0200

    Modificacion en el codigo

--- Practica1/ejercicio1.m
+++ Practica1/ejercicio1.m
@@ -1,10 +1,19 @@
 clear all;
+close all;
 
+% Matriz de entradas
 P = [0.1 0.7 0.8 0.8 1.0 0.3 0.0 -0.3 -0.5 -1.5;
         1.2 1.8 1.6 0.6 0.8 0.5 0.2 0.8 -1.5 -1.3];
+
+% Target
 T = [1 1 1 0 0 1 1 1 0 0;
      0 0 0 0 0 1 1 1 1 1];
 
+% Perceptron
 net = perceptron;
 net = train(net, P, T);
 view(net);
+
+% Muestra de resultados
+plotpv(P, T);
+plotpc(net.IW{1,1}, net.b{1});
