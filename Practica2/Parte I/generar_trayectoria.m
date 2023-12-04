413350b036e69cf8ec50ac23695703ff8ecf3237
mfranck340 <franck.yaguargos@edu.uah.es>
2023-12-04 20:23:25 +0100

    Parte I terminada de momento

--- Practica2/Parte I/generar_trayectoria.m
+++ Practica2/Parte I/generar_trayectoria.m
@@ -1,16 +1,29 @@
-%Tiempo de muestreo
+% Apartado C
+% Tiempo de muestreo
 Ts = 100e-3;
-% Referencia x-y de posicion
-refx = 10*rand-5;
-refy = 10*rand-5;
 
-% Ejecutar Simulacion
-sim('PositionControl.slx')
-
-% Mostrar
-x = salida_x.Data;
-y = salida_y.Data;
 figure;
-plot(x,y);
-grid on;
-hold on
+for i = 1:4
+    % Referencia x-y de posicion
+    refx = 10*rand-5;
+    refy = 10*rand-5;
+    
+    % Ejecutar Simulacion
+    sim('PositionControl.slx')
+    
+    % Mostrar trayectoria
+    x = salida_x.Data;
+    y = salida_y.Data;
+    
+    subplot(2,2,i),plot(x,y);
+    grid on;
+    hold on
+end
+
+%% 
+
+x_0 = 0.02;
+y_0 = 0;
+th_0 = 0;
+
+
--- Practica2/Parte II/generar_circuito.m
+++ Practica2/Parte II/generar_circuito.m
@@ -1,18 +1,25 @@
+% Apartado d)
 %Tiempo de muestreo
 Ts = 100e-3;
+
 % Referencia x-y de posicion
+% Posicion objetivo
 refx = 5;
-refy = 0;
+refy = 5;
+% Posicion del obstaculo
 obsx = 2.5;
-obsy = 0;
+obsy = 2.5;
 
 % Ejecutar Simulacion
 sim('EvitarObstaculo.slx')
 
-% Mostrar
-x=salida_x.signals.values;
-y=salida_y.signals.values;
+% Mostrar trayectoria
+x=salida_x.data;
+y=salida_y.data;
 figure;
 plot(x,y);
 grid on;
-hold on; 
+hold on;
+plot(obsx, obsy, 'x');
+title('Trayectoria del robot');
+legend('Trayectoria', 'Obstaculo');
