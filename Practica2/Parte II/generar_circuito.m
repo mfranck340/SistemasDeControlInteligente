87780cfa6a844878357a6ef6b60998daf9e07b49
mfranck340 <franck.yaguargos@edu.uah.es>
2023-12-10 01:15:49 +0100

    Arreglos en el modelo fuzzy apartado 2

--- Practica2/Parte II/generar_circuito.m
+++ Practica2/Parte II/generar_circuito.m
@@ -4,22 +4,85 @@ Ts = 100e-3;
 
 % Referencia x-y de posicion
 % Posicion objetivo
-refx = 5;
-refy = 5;
+refx = -3.8;
+refy = -4.3;
 % Posicion del obstaculo
-obsx = 2.5;
-obsy = 2.5;
+obsx = 0;
+obsy = -3.38;
 
 % Ejecutar Simulacion
+sim('PositionControl.slx')
+x = salida_x.data;
+y = salida_y.data;
+
 sim('EvitarObstaculo.slx')
+x_obs = salida_x.data;
+y_obs = salida_y.data;
 
 % Mostrar trayectoria
-x=salida_x.data;
-y=salida_y.data;
+figure;
+subplot(1,2,1);
+plot(x,y);
+grid on;
+title('Trayectoria sin obstaculo');
+
+subplot(1,2,2);
+plot(x_obs,y_obs);
+grid on;
+hold on;
+plot(obsx, obsy, 'x');
+title('Trayectoria con obstaculo');
+legend('Trayectoria', 'Obstaculo');
+
+
+
+
+%%
+% Apartado e)
+% Tiempo de muestreo
+Ts = 100e-3;
+
+% Inicio de la trayectoria
+x_0 = 0.015;
+y_0 = 0;
+th_0 = 0;
+
+% Posicion del obstaculo
+obsx = 3;
+obsy = 0;
+
+% Ejecutar Simulacion
+
+
+sim('EvitarObstaculoTrajectory.slx')
+x_obs = salida_x.data;
+y_obs = salida_y.data;
+x = ruta_x.data;
+y = ruta_y.data;
+
+% Mostrar trayectoria
 figure;
 plot(x,y);
 grid on;
 hold on;
-plot(obsx, obsy, 'x');
-title('Trayectoria del robot');
-legend('Trayectoria', 'Obstaculo');
+plot(x_obs,y_obs);
+plot(obsx, obsy, 'gx');
+title('Trayectoria con obstaculo');
+legend("ruta", "robot", "obs");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
