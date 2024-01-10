afb001678597c5e9cca69d87b8d0c29f42bab2c2
mfranck340 <franck.yaguargos@edu.uah.es>
2024-01-10 19:37:50 +0100

    Version red neuronal

--- PracticaFinal/Nuevo/control_neuronal.m
+++ PracticaFinal/Nuevo/control_neuronal.m
@@ -1,18 +1,21 @@
-%{
-%%
-load("variables.mat")
+%% Cargar datos para entrenamiento
+load("variables2.mat")
+
+%% Inicializar datos de entrenamiento
+inputs = [];
+outputs = [];
 
 %% Obtencion datos de entrenamiento
 inputs = [inputs, entradas.data'];
 outputs = [outputs, salidas.data'];
 
-%%
+%% Limpiar datos
 inputs(isinf(inputs)) = 5.0;
 inputs = double(inputs);
 outputs = double(outputs);
-%}
-%%
-net = feedforwardnet([10, 5]);
+
+%% Generar red
+net = feedforwardnet([20 25 20]);
 
 %% Entrenamiento de red
 net = configure(net,inputs,outputs);
@@ -21,5 +24,5 @@ net = train(net,inputs,outputs);
 %% Generar bloque simulink
 gensim(net, 0.1)
 
-%%
-%save("variables.mat", "inputs", "outputs")
+%% Almacenar datos
+save("variables2.mat", "inputs", "outputs")
